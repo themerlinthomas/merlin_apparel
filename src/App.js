793d3b1b98093381496d30bbc6c4858f51838655
themerlinthomas <Merlin.Thomas@ibm.com>
2022-02-18 14:51:07 +0530

    Added homepage styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
       <HomePage />
-
     </div>
   );
 }
