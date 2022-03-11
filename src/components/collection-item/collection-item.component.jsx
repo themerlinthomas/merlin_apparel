77b466dbef2b1a09f9b452279eaa3c8014424ac9
themerlinthomas <Merlin.Thomas@ibm.com>
2022-03-12 01:56:33 +0530

    Added checkout dropdown

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
 import './collection-item.styles.scss';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+    const {name, price, imageUrl} = item;
+    return(
     <div className='collection-item'>
         <div className='image'
             style={{ 
@@ -11,7 +16,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton inverted onClick={() => addItem(item)} >Add to Cart</CustomButton>
     </div>
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem : item => dispatch(addItem(item))
+})
+export default connect(null,mapDispatchToProps)(CollectionItem);
